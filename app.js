009a67baf95c87fcfaee319fbfa33308e515304f
Uliaaan <markovopg@gmail.com>
2018-06-21 14:40:54 +1000

    node base server

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+app.get('/', (req, res) => {
+  res.send(`It work!`)
+})
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+  console.log(`Server start on port ${port}`)
+})
