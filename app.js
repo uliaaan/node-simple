7d237af9c1824441db6260b0b94dcb44d3b7cd43
Uliaaan <markovopg@gmail.com>
2018-06-23 01:04:37 +1000

    Add model and Goauth

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const passport = require('passport')
+
+//Load User Model
+require('./models/User')
+
+//Passport Config
+require('./config/passport')(passport)
+
+//Load Routes
+const auth = require('./routes/auth')
+
+//Load Keys
+const keys = require('./config/keys')
+
+//Map global promises
+mongoose.Promise = global.Promise
+
+//Mongoose Connect
+mongoose.connect(keys.mongoURI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err))
 
 const app = express()
 app.get('/', (req, res) => {
   res.send(`It work!`)
 })
+
+//Use Routes
+app.use('/auth', auth)
 
 const port = process.env.PORT || 5000
 
