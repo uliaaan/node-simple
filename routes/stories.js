7303cac5410044bae3bceb5c211d78807d3c9324
Uliaaan <markovopg@gmail.com>
2018-06-26 19:43:27 +1000

    Edit func

--- routes/stories.js
+++ routes/stories.js
@@ -35,6 +35,18 @@ router.get('/add', ensureAuthenticated, (req, res) => {
   res.render(`stories/add`)
 })
 
+//Edit Story Form
+router.get('/edit/:id', ensureAuthenticated, (req, res) => {
+  Story.findOne({
+    _id: req.params.id
+  })
+  .then(story => {
+    res.render('stories/edit', {
+      story: story
+    })
+  })
+})
+
 //Process Add Story
 router.post('/', (req, res) => {
   let allowComments
@@ -42,7 +54,7 @@ router.post('/', (req, res) => {
   if(req.body.allowComments) {
     allowComments = true
   } else {
-    allowComments - false
+    allowComments = false
   }
 
   const newStory = {
@@ -59,7 +71,33 @@ router.post('/', (req, res) => {
   .then(story => {
     res.redirect(`/stories/show/${story.id}`)
   })
+})
 
+//Edit Form Process
+router.put('/:id', (req, res) => {
+  Story.findOne({
+    _id: req.params.id
+  })
+  .then(story => {
+    let allowComments
+
+    if(req.body.allowComments) {
+      allowComments = true
+    } else {
+      allowComments = false
+    }
+
+    //New values
+    story.title = req.body.title
+    story.body = req.body.body
+    story.status = req.body.status
+    story.allowComments = allowComments
+    
+    story.save()
+      .then(story => {
+        res.redirect('/dashboard')
+      })
+  })
 })
 
 module.exports = router
